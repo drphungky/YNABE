1c3f03bd25309f54146ba2d232426deeaf119fef
drphungky <59374030+drphungky@users.noreply.github.com>
2021-11-22 21:30:53 -0500

    Wrote to JSON

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-# This is a sample Python script.
-
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+import requests
+import json
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+def connect():
+
+    headers = {
+        'Authorization': 'Bearer 7d61f7b763a9c0122580e5ec18e9007862bacbc032200c8ccc4c064cff982844',
+    }
+
+    response = requests.get('https://api.youneedabudget.com/v1/budgets', headers=headers)
+    d=response.json()["data"]
+    print(d)
+    with open("output.json", 'w') as outfile:
+        json.dump(d, outfile, indent=4)
+
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Erik2')
+    connect()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
